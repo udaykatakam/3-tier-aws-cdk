834df45253969c42a86f718f45bd0231192478b5
udaykatakam <udaikiran@gmail.com>
2022-04-25 22:42:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from 3Tier.stack import MyStack
 
 app = core.App()
 
-stack = MyStack(app, "3Tier-cdk", env={'account': '627796554250', 'region': 'us-west-2'})
+stack = MyStack(app, "3Tier-cdk", env={'account': '627796554250', 'region': 'us-west-1'})
 
 core.Tag.add( stack, key='Project Code', value='Project Code')
 
