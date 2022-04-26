8aca5d51856ac6ca4b85a099440828d9949e2a67
Uday Katakam <udaykatakam@Udays-Mac.local>
2022-04-25 22:43:39 -0700

    Testing

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from aws_cdk import core
 from aws_cdk.core import Tag
 from 3Tier.stack import MyStack
 
-
 app = core.App()
 
-stack = MyStack(app, "3Tier-cdk", env={'account': '627796554250', 'region': 'us-west-2'})
+stack = MyStack(app, "3Tier-cdk",
+                env={'account': '627796554250', 'region': 'us-west-2'})
 
-core.Tag.add( stack, key='Project Code', value='Project Code')
+core.Tag.add(stack, key='Project Code', value='Project Code')
 
 app.synth()
